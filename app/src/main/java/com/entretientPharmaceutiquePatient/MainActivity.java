781dc25e9a62cf535cb0f9f8cc5bc81fce350798
Tom <tom59880@hotmail.fr>
2021-05-06 08:30:25 +0200

    Material Components for Android

--- app/src/main/java/com/entretientPharmaceutiquePatient/MainActivity.java
+++ app/src/main/java/com/entretientPharmaceutiquePatient/MainActivity.java
@@ -1,13 +1,25 @@
 package com.entretientPharmaceutiquePatient;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Information info = new Information();
+        //FragmentManager fm = getSupportFragmentManager();
+
+
+
     }
+
+
 }
