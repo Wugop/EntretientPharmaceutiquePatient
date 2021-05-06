6b7e0899c6a200db82bf3b8f9f710aa3c80726e0
Tom <tom59880@hotmail.fr>
2021-05-06 10:04:39 +0200

    Implementation Material Components

--- app/src/main/java/com/entretientPharmaceutiquePatient/MainActivity.java
+++ app/src/main/java/com/entretientPharmaceutiquePatient/MainActivity.java
@@ -8,16 +8,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button button;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Information info = new Information();
-        //FragmentManager fm = getSupportFragmentManager();
-
-
 
     }
 
